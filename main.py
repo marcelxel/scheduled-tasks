85ee3f37edcf19ba4d058c4754fddceb267ac1f2
marcelxel <marcelhb93@gmail.com>
2026-04-29 22:06:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,26 +13,24 @@ import smtplib
 import os
 
 # import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+my_email_1 = os.environ.get("MY_EMAIL")
+password_1 = os.environ.get("MY_PASSWORD")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+# 1. Update the birthdays.csv
+df_birthdays = pandas.read_csv("birthdays.csv")
+birthdays_dict = {(data_row.month,data_row.day):data_row for (index, data_row) in df_birthdays.iterrows()}
+# 2. Check if today matches a birthday in the birthdays.csv
+today = (dt.datetime.now().month,dt.datetime.now().day)
+if today in birthdays_dict:
+    # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+    with open(f"./letter_templates/letter_{random.randint(1,3)}.txt", "r") as file:
+        letter = file.read()
+        name = birthdays_dict[today]["name"]
+        letter = letter.replace("[NAME]",name)
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
-
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+    # 4. Send the letter generated in step 3 to that person's email address.
+    email_2 = birthdays_dict[today]["email"]
+    with smtplib.SMTP("smtp.gmail.com") as connexion:
+        connexion.starttls()
+        connexion.login(user=my_email_1,password=password_1)
+        connexion.sendmail(from_addr=my_email_1,to_addrs=email_2,msg=f"Subject: Happy Birthday!\n\n{letter}")
